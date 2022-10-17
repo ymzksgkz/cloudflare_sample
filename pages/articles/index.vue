29e7fbf7b3d34afe5b2070293e8e0b82f180927c
Shigekazu Yamazaki <83740740+shigekazu-branu@users.noreply.github.com>
2022-10-17 17:05:55 +0900

    記事一覧作成

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <span>記事一覧</span>
+  <div>
+    <span>記事一覧</span>
+    <div class="article" v-for="article in contents">
+      <NuxtLink :to="`/articles/${article.id}`" >
+        <span>{{ article.title }}</span>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage'
+  name: 'ArticleList',
+  data() {
+    return {
+      contents: {}
+    }
+  }, async created() {
+    const { data: { contents } } = await axios.get(
+        `https://cms-practice.microcms.io/api/v1/news`,
+        {
+          // TODO 環境変数にしたほうがよいけど一旦いい。
+          headers: {'X-MICROCMS-API-KEY': 'a85dd8764e99460e92d2bdf73906732c1a56'}
+        }
+    )
+    this.contents = contents
+  }
 }
 </script>
+<style lang="css">
+.article {
+  margin: 10px;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
